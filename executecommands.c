bcce068c76e58286c6c02165421510d2f808c720
aymen723 <benmohammed.a.a@gmail.com>
2023-07-28 01:14:16 +0100

    test

--- executecommands.c
+++ executecommands.c
@@ -2,27 +2,29 @@
 
 void executecommands(char **argv)
 {
-  char *command = NULL;
-  pid_t child_pid;
+      char *command = NULL;
+	pid_t child_pid;
 
     if (argv){
         command = argv[0];
 
 
-        child_pid = fork();
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("Error:");
 
-        if (child_pid < 0) {
-            perror("Error:");
-            return;
-        }
-        else if (child_pid == 0) {
-            if (execve(command, argv, NULL) == -1){
-                perror("Error:");
-            }
-        }
-        else {
-            int status;
-            wait(&status);
-        }
+		}
+
+
+       
     }
+}
 }
